c4facd18cdeeb17c243291549f2537a76b6668ff
John 22 <145293551+John23ee@users.noreply.github.com>
2025-11-15 13:48:52 +0200

    Delete insane-site directory

--- insane-site/service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('index.html')) {
-    e.respondWith(fetch(e.request));
-  }
-});
